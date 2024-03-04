94564490a505df2ce17f9cb935e786052863a586
c9yu <polkj11290@naver.com>
2024-03-04 17:04:17 +0900

    20240304 오후 완료

--- day08/day08/day08/test12RealObjUnder2.cpp
+++ day08/day08/day08/test12RealObjUnder2.cpp
@@ -4,17 +4,23 @@ using namespace std;
 typedef struct Data
 {
 	int data;
-	void(*ShowData)(Data*);
-	void(*Add)(Data*, int);
+	void(*ShowData)(Data*);   // ShowData 멤버함수 선언   void(함수주소)()
+	void(*Add)(Data*, int);   // Add      멤버함수 선언
 };	Data;
 
 
-void ShowData(Data* THIS) { cout << "Data: " << THIS->data << endl; }
-void (*Add)(Data* THIS, int num) { THIS->data += num; }
+void ShowData(Data* THIS)     // ShowData 멤버함수 정의
+{ 
+	cout << "Data: " << THIS->data << endl; 
+}
+void Add(Data* THIS, int num) // Add 멤버함수 정의
+{ 
+	THIS->data+= num; 
+}
 
 int main(void)
 {
-	Data obj1 = { 15, ShowData,Add };
+	Data obj1 = { 15, ShowData,Add }; // 구조체 Data 변수 obj1 선언과 초기화
 	Data obj2 = { 7, ShowData,Add };
 
 	obj1.Add(&obj1, 17);
