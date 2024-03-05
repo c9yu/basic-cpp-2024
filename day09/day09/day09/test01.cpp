afc58bbec170452dedb28010ddb251a96a0ece9c
c9yu <polkj11290@naver.com>
2024-03-05 12:31:55 +0900

    20240305

--- day09/day09/day09/test01.cpp
+++ day09/day09/day09/test01.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -13,10 +14,10 @@ public:
 	{
 		cout << '[' << xpos << ", " << ypos << ']' << endl;
 	}
-	Point operator+(const Point& ref)
+	Point operator+(const Point& ref) // operator+라는 이름의 멤버 함수
 	{
-		Point pos(xpos + ref.xpos, ypos + ref.ypos);
-		return pos;
+		Point pos(xpos + ref.xpos, ypos + ref.ypos); // 새로운 포인터 객체(pos) 생성
+		return pos;                                  // 만든 자료형에 + 하기 위해서 연산자 오버로딩을 통해 더하기 기능을 구현해준다.
 	}
 };
 
@@ -24,10 +25,13 @@ int main(void)
 {
 	Point pos1(3, 4);
 	Point pos2(10, 20);
-	Point pos3 = pos1.operator+(pos2);
+	Point pos3 = pos1.operator+(pos2);  // 만든 operator+를 사용하는 방법
+	Point pos4 = pos2.operator+(pos3);  // 먄약 operator-를 만들었다면, pos2.operator-(pos3)의 형태였을 것이다.
 
 	pos1.ShowPosition();
 	pos2.ShowPosition();
 	pos3.ShowPosition();
+	pos4.ShowPosition();
 	return 0;
 }
+*/
